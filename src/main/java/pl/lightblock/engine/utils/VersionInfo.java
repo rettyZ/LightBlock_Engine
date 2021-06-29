9d85899ea78ad574471019c6b66af67e9cd51aa2
Bartosz Bart <cookiedesign.fx@gmail.com>
2021-06-29 16:42:34 +0200

    457

---
+++ src/main/java/pl/lightblock/engine/utils/VersionInfo.java
@@ -0,0 +1,10 @@
+package pl.lightblock.engine.utils;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class VersionInfo {
+    public static String ServerVersion = "1.0";
+    public static Date ReleaseDate = new Date(2021, Calendar.JUNE, 29);
+    public static String Changelog = "No, pierwsza wersja";
+}
